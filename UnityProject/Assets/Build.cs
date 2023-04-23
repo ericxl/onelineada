52b0d84e3dba8af2c645412141aefabfaa3968ff
Eric Liang <jklelxy@gmail.com>
2023-04-23 12:16:11 -0700

    Added build script

---
+++ UnityProject/Assets/Build.cs
@@ -0,0 +1,38 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class Build : MonoBehaviour
+{
+    [MenuItem("Build/Build project %#U")]
+    private static void BuildProject()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+
+        //Debug.LogError(Application.dataPath);
+
+        var path = Path.Combine(Path.GetDirectoryName(Application.dataPath), "iOSBuild");
+        buildPlayerOptions.locationPathName = path;
+        buildPlayerOptions.target = BuildTarget.iOS;
+
+        Directory.CreateDirectory(buildPlayerOptions.locationPathName);
+        //buildPlayerOptions.locationPathName = path;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.LogError("Build failed: " + summary.result.ToString());
+        }
+    }
+}
+
+#endif
