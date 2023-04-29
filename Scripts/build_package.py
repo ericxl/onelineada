3f535c9f32003f75b03256e237dda3b82709ec11
Eric Liang <jklelxy@gmail.com>
2023-04-29 12:40:53 -0700

    Updated directory

---
+++ Scripts/build_package.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+
+# print("helo" + os.environ["BUILT_PRODUCTS_DIR"])
+
+if os.path.exists(os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin")):
+    shutil.rmtree(os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin"))
+shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Runtime"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityObjC/Runtime"))
+shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Source"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityObjC/Runtime/iOS"))
+shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityAXUtils/Source"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityAXUtils/Runtime/iOS"))
+# shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Source"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityObjC/Runtime/iOS"))
+# shutil.copytree(os.environ["BUILT_PRODUCTS_DIR"], os.path.join(os.getcwd(),  os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/Runtime/iOS")))
+# shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityAXUtils"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityAXUtils"))
+shutil.copytree(os.path.join(os.getcwd(), "Accessibility"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/Accessibility/Runtime/iOS"))
+with open(os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/package.json"), "w") as file:
+    file.write("""{
+  "name": "com.xiaoyongliang.iosunityaccessibilityplugin",
+  "displayName": "iOSUnityAccessibilityPlugin",
+  "description": "Provides accessibility for Unity game",
+  "version": "1.0.0",
+  "unity": "2019.2",
+  "keywords": [
+    "accessibility"
+  ],
+  "author": {
+    "name": "Xiaoyong Liang",
+    "email": "jklelxy@gmail.com",
+    "url": "https://www.xiaoyongliang.com"
+  }
+}""")
