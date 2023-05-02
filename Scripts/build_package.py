264705344fb15f46f9a7505373b72843ae923759
Eric Liang <jklelxy@gmail.com>
2023-05-02 12:15:25 -0700

    preparing for demo

--- Scripts/build_package.py
+++ Scripts/build_package.py
@@ -4,35 +4,34 @@ import os
 import sys
 import shutil
 
-include_source = False;
-plugin_folder = os.path.join(os.getcwd(), "unity_package/iOSUnityAccessibilityPlugin");
+include_source = True;
+xcodeproj_name = next((item for item in os.listdir(os.getcwd()) if os.path.isdir(os.path.join(os.getcwd(), item)) and item.endswith('.xcodeproj')), None).rstrip(".xcodeproj")
+plugin_name = f"{xcodeproj_name}Plugin"
+plugin_folder = os.path.join(os.getcwd(), f"unity_package/{plugin_name}");
 if os.path.exists(plugin_folder):
   shutil.rmtree(plugin_folder)
 
 if include_source:
-  shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Runtime"), os.path.join(plugin_folder, "UnityObjC/Runtime"))
-  shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityAXUtils/Runtime/iOS"), os.path.join(plugin_folder, "UnityAXUtils/Runtime/iOS"))
-  shutil.copytree(os.path.join(os.getcwd(), "Accessibility/Runtime/iOS"), os.path.join(plugin_folder, "Accessibility/Runtime/iOS"))
+  shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Runtime"), os.path.join(plugin_folder, "Accessibility/UnityObjC/Runtime"))
+  shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityAXUtils/Runtime"), os.path.join(plugin_folder, "Accessibility/UnityAXUtils/Runtime"))
+  shutil.copytree(os.path.join(os.getcwd(), "Accessibility/Runtime"), os.path.join(plugin_folder, "Accessibility/Runtime"))
 else:
   shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Runtime"), os.path.join(plugin_folder, "Runtime"))
   shutil.rmtree(os.path.join(plugin_folder, "Runtime/iOS"))
   shutil.copytree(os.environ["BUILT_PRODUCTS_DIR"], os.path.join(plugin_folder, "Runtime/iOS"))
-# shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityObjC/Source"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityObjC/Runtime/iOS"))
-# 
-# shutil.copytree(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())), "Plugins/UnityAXUtils"), os.path.join(os.getcwd(), "Packages/iOSUnityAccessibilityPlugin/UnityAXUtils"))
+
 with open(os.path.join(plugin_folder, "package.json"), "w") as file:
-    file.write("""{
-  "name": "com.xiaoyongliang.iosunityaccessibilityplugin",
-  "displayName": "iOSUnityAccessibilityPlugin",
-  "description": "Provides accessibility for Unity game",
+    file.write(f'''{{
+  "name": "com.ai.revui.unityplugin.{xcodeproj_name.lower()}",
+  "displayName": , "{plugin_name}"
+  "description": "Provides iOS accessibility for this project",
   "version": "1.0.0",
   "unity": "2019.2",
   "keywords": [
     "accessibility"
   ],
-  "author": {
-    "name": "Xiaoyong Liang",
-    "email": "jklelxy@gmail.com",
-    "url": "https://www.xiaoyongliang.com"
-  }
-}""")
+  "author": {{
+    "name": "revui AI",
+    "url": "https://revui.ai"
+  }}
+}}''')
